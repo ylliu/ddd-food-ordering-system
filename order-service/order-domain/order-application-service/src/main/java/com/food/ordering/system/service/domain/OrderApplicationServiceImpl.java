19458675fecdb6301104852468ee2835009aea5e
ylliu <liuming1908@gmail.com>
2023-03-08 14:58:45 +0800

    20. Implementing input ports in Order Application Service

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/service/domain/OrderApplicationServiceImpl.java
@@ -0,0 +1,34 @@
+package com.food.ordering.system.service.domain;
+
+import com.food.ordering.system.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.service.domain.ports.input.service.OrderApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+class OrderApplicationServiceImpl implements OrderApplicationService {
+
+    private final OrderCreateCommandHandler orderCreateCommandHandler;
+    private final OrderTrackCommandHandler orderTrackCommandHandler;
+
+    public OrderApplicationServiceImpl(OrderCreateCommandHandler orderCreateCommandHandler, OrderTrackCommandHandler orderTrackCommandHandler) {
+        this.orderCreateCommandHandler = orderCreateCommandHandler;
+        this.orderTrackCommandHandler = orderTrackCommandHandler;
+    }
+
+    @Override
+    public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
+        return orderCreateCommandHandler.createOrder(createOrderCommand);
+    }
+
+    @Override
+    public TrackOrderResponse trackOder(TrackOrderQuery trackOrderQuery) {
+        return orderTrackCommandHandler.trackOrder(trackOrderQuery);
+    }
+}
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/service/domain/ports/input/service/OrderApplicationService.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/service/domain/ports/input/service/OrderApplicationService.java
@@ -3,11 +3,12 @@ package com.food.ordering.system.service.domain.ports.input.service;
 import com.food.ordering.system.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.service.domain.dto.track.TrackOrderResponse;
 
 import javax.validation.Valid;
 
 public interface OrderApplicationService {
     CreateOrderResponse createOrder(@Valid CreateOrderCommand createOrderCommand);
 
-    TrackOrderQuery trackOder(@Valid TrackOrderQuery trackOrderQuery);
+    TrackOrderResponse trackOder(@Valid TrackOrderQuery trackOrderQuery);
 }
